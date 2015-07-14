1d9c8231aa6d170a89a8be0c63e0a106d6ed75fc
dave45678 <dave45678@gmail.com>
2015-07-14 11:03:23 -0400

    First Commit

---
+++ CreateJSTLTest/src/Danny.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/Danny")
+public class Danny extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Danny() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String danny = "crazy";
+		request.setAttribute("message", danny);
+		request.getServletContext().getRequestDispatcher("/Welcome.jsp").forward(request, response);;
+	}
+}
